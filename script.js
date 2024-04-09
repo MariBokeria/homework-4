41d8e7f13365c666ba6ea4a22ecfaede8c9d1414
MariBokeria <mariambokeria19@gamil.com>
2024-04-10 00:12:46 +0400

    homework

---
+++ script.js
@@ -0,0 +1,34 @@
+// let temp = 25; 
+//type here any temperature
+
+// if (temp <=0 ){
+//     console.log("Very Cold");
+// }
+// else if(temp >=0 && temp <10){
+//     console.log("Cold");
+// }
+// else if(temp >=10 && temp <20){
+//     console.log("Mild");
+// }
+// else if(temp >=20 && temp <30){
+//     console.log("Warm");
+// }
+// else{
+//     console.log("Hot");
+// }
+
+let gender= "male";
+
+switch(gender){
+    case "male":
+        console.log("male")
+    break;
+    case "female":
+        console.log("female")
+    break;
+    case "other":
+        console.log("other")
+    break;
+    default: 
+        console.log("ERROR SOMETHING WENT WRONG!.")
+}
